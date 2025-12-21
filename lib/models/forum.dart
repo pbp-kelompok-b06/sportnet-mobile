63b7564a9d986ddf27bb78de717367fb9e7a8c08
RayhanDerya <rayhanderya9@gmail.com>
2025-12-21 22:16:26 +0700

    add edit delete forum

--- lib/models/forum.dart
+++ lib/models/forum.dart
@@ -3,12 +3,14 @@ class ForumPost {
   final String author;
   final String message;
   final DateTime createdAt;
+  final bool isOwner;
 
   ForumPost({
     required this.id,
     required this.author,
     required this.message,
     required this.createdAt,
+    required this.isOwner,
   });
 
   factory ForumPost.fromJson(Map<String, dynamic> json) {
@@ -17,6 +19,7 @@ class ForumPost {
       author: json['username'],
       message: json['content'],
       createdAt: DateTime.parse(json['created_at']),
+      isOwner: json['is_owner'] ?? false,
     );
   }
 }
